395ebc8bcfce98ee8a1beecbd51a733591a7d5bc
Walter Reiner <walter.reiner@wreiner.at>
2024-02-26 10:05:37 +0100

    Remove unnecessary functions

--- abgabe01/app/helper/read.js
+++ abgabe01/app/helper/read.js
@@ -1,53 +1,5 @@
 const fs = require('fs');
 
-function readFilePromise(filePath) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-            } else {
-                console.log(data);
-                resolve(data);
-            }
-        });
-    });
-}
-
-function readTextFile(filePath, callback) {
-    console.log('readFile() ' + filePath);
-    readFilePromise(filePath)
-        .then((data) => {
-            callback(null, data);
-        })
-        .catch((err) => {
-            callback(err);
-        });
-}
-
-function readImageFile(filePath, callback) {
-    console.log('readImageFile() ' + filePath);
-    readImageFilePromise(filePath)
-        .then((data) => {
-            callback(null, data);
-        })
-        .catch((err) => {
-            callback(err);
-        });
-}
-
-function readImageFilePromise(filePath) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
-    });
-}
 
 function readFilesPromise(filePath, callback) {
     console.log('readFiles() ' + filePath);
