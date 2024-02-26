820e7a80d7c0d304d29b272174cfd13dc13ddc52
Walter Reiner <walter.reiner@wreiner.at>
2024-02-26 10:04:59 +0100

    Refactor for less code

--- abgabe01/app/helper/read.js
+++ abgabe01/app/helper/read.js
@@ -14,7 +14,7 @@ function readFilePromise(filePath) {
     });
 }
 
-function readFile(filePath, callback) {
+function readTextFile(filePath, callback) {
     console.log('readFile() ' + filePath);
     readFilePromise(filePath)
         .then((data) => {
@@ -49,7 +49,48 @@ function readImageFilePromise(filePath) {
     });
 }
 
+function readFilesPromise(filePath, callback) {
+    console.log('readFiles() ' + filePath);
+
+    if (filePath.endsWith('.css') || filePath.endsWith('.js') || filePath.endsWith('.html')) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(filePath, 'utf8', (err, data) => {
+                if (err) {
+                    console.error(err);
+                    reject(err);
+                } else {
+                    console.log(data);
+                    resolve(data);
+                }
+            });
+        });
+    }
+
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+function readFile(filePath, callback) {
+    console.log('readFile() ' + filePath);
+    readFilesPromise(filePath)
+        .then((data) => {
+            callback(null, data);
+        })
+        .catch((err) => {
+            callback(err);
+        });
+}
+
 module.exports = {
     readFile: readFile,
-    readImageFile: readImageFile
+    readImageFile: readImageFile,
+    readTextFile: readTextFile,
 };
--- abgabe01/app/server.js
+++ abgabe01/app/server.js
@@ -1,64 +1,44 @@
 const http = require('http');
-const fs = require('fs');
 const path = require('path');
-const customReadModule = require('./helper/read.js');
 const url = require('url');
+
+const customReadModule = require('./helper/read.js');
 
 
 const server = http.createServer((req, res) => {
     const urlstring = url.parse(req.url, true);
     console.log(urlstring);
+    console.log('Request: ' + req.url);
     const resourcePath = path.join(__dirname, '..', 'public');
 
-    if (urlstring.path.includes('css')) {
-        console.log('css');
-        customReadModule.readFile(resourcePath + req.url, (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/css' });
-                res.end('Internal server error');
-            } else {
-                res.writeHead(200);
-                res.end(data);
-            }
-        });
-    } else if (urlstring.path.includes('js')) {
-        console.log('js');
-        customReadModule.readFile(resourcePath + req.url, (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/javascript' });
-                res.end('Internal server error');
-            } else {
-                res.writeHead(200);
-                res.end(data);
-            }
-        });
-    } else if (urlstring.path.includes('html')) {
-        console.log('html');
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        customReadModule.readFile(resourcePath + '/index.html', (html) => res.end(html));
-    } else if (urlstring.path.includes('png')) {
-        console.log('png');
-        customReadModule.readImageFile(resourcePath + req.url, (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'image/png' });
-                res.end('Internal server error');
-            } else {
-                res.writeHead(200, { 'Content-Type': 'image/png' });
-                res.end(data);
-            }
-        });
-    } else {
-        console.log('else');
-        customReadModule.readFile(resourcePath + '/index.html', (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Internal server error');
-            } else {
-                res.writeHead(200);
-                res.end(data);
-            }
-        });
+    let contentType = 'text/html';
+    switch (path.extname(req.url)) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
     }
+
+    let requestPath = req.url;
+    if (requestPath === '/') {
+        requestPath = '/index.html';
+    }
+
+    console.log('contentType: ' + contentType);
+    customReadModule.readFile(resourcePath + requestPath, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': contentType });
+            res.end('Internal server error');
+        } else {
+            res.writeHead(200);
+            res.end(data);
+        }
+    });
 });
 
 server.listen(3000, () => {
